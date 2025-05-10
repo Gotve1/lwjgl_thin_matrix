0baa8d7b6556d64b93248d50945eea06f5fe8e0f
Sunrise <akselvolkov40@gmail.com>
2025-05-10 09:11:16 +0500

    Add index buffer support for VAO loading and rendering

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,14 +14,15 @@ public class Main {
                 -0.5f,  0.5f, 0f,  // Top left vertex
                 -0.5f, -0.5f, 0f,  // Bottom left vertex
                  0.5f, -0.5f, 0f,  // Bottom right vertex
-
-                 // Second triangle X, Y, Z
-                 0.5f, -0.5f, 0f,  // Bottom right vertex (repeated)
-                 0.5f,  0.5f, 0f,  // Top right vertex
-                -0.5f,  0.5f, 0f   // Top left vertex (repeated)
+                 0.5f,  0.5f, 0f   // Top right vertex
         };
 
-        RawModel model = loader.loadToVAO(vertices);
+        int[] indices = {
+                0, 1, 3, // First triangle
+                3, 1, 2  // Second triangle
+        };
+
+        RawModel model = loader.loadToVAO(vertices, indices);
 
         while (!Display.isCloseRequested()) {
             render.init();
